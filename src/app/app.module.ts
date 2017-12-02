7da021118bfcd3b5c5ce86dab0636c91ceddcabb
chris <zrzllyz0@gmail.com>
2017-12-02 18:44:03 -0500

    change the bug in app.module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from 'modulear/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -6,20 +6,20 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
+import { UserService } from './user.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent,
-    HeroService
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [HeroService, MessageService],
+  providers: [HeroService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
