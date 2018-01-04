a2fe5789ed81a42b555922d09514e05a03361f31
rz73506 <runze.zhang>
2018-01-04 16:51:19 -0500

    Finished routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,9 @@
-import {Injectable} from '@angular/core';
-import {Hero} from './hero';
-import {HEROES} from './mock-heroes';
-import {Observable} from 'rxjs/Observable';
-import {of} from 'rxjs/observable/of';
-import {MessageService} from './message.service';
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
@@ -15,6 +15,11 @@ export class HeroService {
     // Todo: send the message_after_fetching the heroes
     this.messageService.add('HereService: get heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 
 }
