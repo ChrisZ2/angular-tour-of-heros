62639ae6ccf90fd0b5d48337b71412c157084475
rz73506 <runze.zhang>
2018-01-04 15:21:14 -0500

    Finished the first route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeroService } from './hero.service';
 import { UserService } from './user.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { MessageService } from './message.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [HeroService, UserService, MessageService],
   bootstrap: [AppComponent]
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -11,6 +11,14 @@ export class MessageService {
     this.messages.push(message);
   }
 
+  getLast() {
+    if (this.messages.length === 0) {
+      return "No Message";
+    }else {
+      return this.messages[this.messages.length - 1];
+    }
+  }
+
   clear() {
     this.messages = [];
   }
